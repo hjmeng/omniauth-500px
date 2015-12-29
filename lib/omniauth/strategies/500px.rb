902df6f59ed0955ed7bfd434bf7d8a75c5839aef
hjmeng <hjmeng@tovide.com>
2015-12-29 01:48:57 -0800

    rename to f00px to satisfy ruby naming convention

--- lib/omniauth/strategies/500px.rb
+++ lib/omniauth/strategies/500px.rb
@@ -6,7 +6,7 @@ module OmniAuth
 
     class FiveHundredPx < OmniAuth::Strategies::OAuth
       
-      option :name, '500px'
+      option :name, 'f00px'
       
       option :client_options, {
         :access_token_path => "/v1/oauth/access_token",
